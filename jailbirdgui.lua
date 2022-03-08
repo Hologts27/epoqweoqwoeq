9ac151345c0806ce3b6060839d6d8b40f0eca2a4
Hologts27 <62165993+Hologts27@users.noreply.github.com>
2022-03-08 20:00:02 -0300

    Update jailbirdgui.lua

--- jailbirdgui.lua
+++ jailbirdgui.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 6055959032 then
-    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Hologts27/epoqweoqwoeq/lobby.lua'))()
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Hologts27/epoqweoqwoeq/main/lobby.lua'))()
 elseif game.PlaceId == 6452089755 then
-    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Hologts27/epoqweoqwoeq/playing.lua'))()
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Hologts27/epoqweoqwoeq/main/playing.lua'))()
 end
