caa403d33d334d10d593c448c0e4000b23263ae0
Hologts27 <62165993+Hologts27@users.noreply.github.com>
2022-03-07 21:45:29 -0300

    Create jailbirdgui.lua

---
+++ jailbirdgui.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 6055959032 then
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/RevertSucks/literacy/main/games/jailbird/lobby.lua'))()
+elseif game.PlaceId == 6452089755 then
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/RevertSucks/literacy/main/games/jailbird/playing.lua'))()
+end
